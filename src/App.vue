dda9617b96111defe089a97ee2752d02dd8a6c2f
k <developerlyk@gmail.com>
2024-03-10 21:36:56 +0900

    Composition api emit사용

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <childComponent
-    :sendProps1="title"
-    :sendProps2="createAt"
-    :sendProps3="obj"
-  />
+  부모컴포넌트 레이아웃
+  <!-- 자식컴포넌트에서 emit한걸 send-even로 받는다 parentEvent를 실행한다 -->
+  <childComponent @send-event="parentEvent" />
 </template>
 <!-- 컴포지션 api는 setup이란게 붙어야함 -->
 <script setup lang="ts">
-import { reactive, ref } from "vue";
 import ChildComponent from "./components/ChildComponent.vue";
 
-interface Obj {
-  id: number;
-  name: string;
-}
-
-const title = ref("부모컴포넌트에서 선언된 데이터입니다.");
-const createAt = ref<nubmer>(2024);
-const obj = reactive<Obj>({
-  id: 2024,
-  name: "john",
-});
+const parentEvent = (event: string) => {
+  console.log(event);
+};
 </script>
 
 <style></style>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,33 +1,15 @@
 <template>
-  <div>{{ sendProps1 }}</div>
-  <div>{{ sendProps2 }}</div>
-  <div>{{ sendProps3.id }}</div>
-  <div>{{ sendProps3.name }}</div>
+  <button @click="sendEvent">자식컴포넌트에서 만든 버튼</button>
 </template>
 
 <script setup lang="ts">
-import { toRefs } from "vue";
+import { ref } from "vue";
 
-interface Obj {
-  id: number;
-  name: string;
-}
-
-//1.컴포지션 api에서 타입스크립트를 활용하는 방법
-//1-1 prop 받은 데이터 형식에 맞게 타입을 지정해준다
-
-//Prop 받은 데이터 이름 자체에 미리 선언해둔 타입을 설정해준다.
-interface Props {
-  sendProps1: String;
-  sendProps2: Number;
-  sendProps3: Obj;
-}
-
-//definProps toRefs 내장함수를 통해 Prop 받은 데이터를 활용하여 템플릿 부분에 호출하여 사용한다.
-/* define props는 import안해도 vue가 알아서 해준다 */
-const props = defineProps<Props>();
-//torefs를 안하면 template에서 props.sendProps1 이런식으로 써야한다
-const { sendProps1, sendProps2, sendProps3 } = toRefs(props);
+const data = ref<string>("자식 컴포넌트에서 선언된 데이터입니다");
+const emit = defineEmits(["send-event"]);
+const sendEvent = () => {
+  emit("send-event", data.value);
+};
 </script>
 
 <style></style>
