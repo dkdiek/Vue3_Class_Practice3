d3a19804f9f4f83d29a7fb566b5c0d94eed023a0
k <developerlyk@gmail.com>
2024-03-10 17:44:17 +0900

    선언적 렌더링, 스타일 클래스 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,40 @@
 <template>
-  <div>
-    {{ count }}
-  </div>
-  <h1>Vue js 라이프사이클 테스트</h1>
+  <!-- 선언적 렌더링 -->
+  <div>{{ rawHtml }}</div>
+  <div>{{ rawHtml2 }}</div>
+  <!-- html 코드를 적용시켜 출력하려면 v-html 디렉티브사용 -->
+  <div v-html="rawHtml2"></div>
+  <!-- v-bind class isActive 변수가 true면 active class 적용 false면 미적용-->
+  <h2 :class="{ active: isActive }">111</h2>
+  <button @click="change">버튼</button>
+  <!-- 스타일바인딩 -->
+  <h3 style="color: red; font-size: 24px">스타일 바인딩 테스트</h3>
+  <h3 :style="{ color: fontColor, fontSize: fontSize + 'px' }">
+    스타일 바인딩 테스트
+  </h3>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 0,
+      rawHtml: "이것은 텍스트입니다",
+      rawHtml2: "<span style='color: red'>이것은 빨간색이어야 합니다.</span>",
+      isActive: false,
+      fontColor: "blue",
+      fontSize: 48,
     };
   },
-  //컴포넌트 생성전 data, mehtods에 선언한 데이터,함수에 접근불가
-  beforeCreate() {
-    console.group("lifcycle is beforecreate", this.count);
-  },
-  //컴포넌트가 생성된 직후 접근할 수 있는 라이프 사이클 훅. 초기화 이후 data접근, 메소드 접근가능
-  created() {
-    console.group("lifcycle is created", this.count);
-    this.test();
-  },
-  //mounted 컴포넌트,템플릿,렌더링된 dom에 접근 가능 dom 수정을 위해 사용 template 부분의 hteml element가 모두 렌더링된 후에 접근 가능
-  //h1 셀렉트 못함
-  // beforeMount() {
-  //   console.group("lifcycle is beforeMount", document.querySelector("h1"));
-  // },
-  // //h1 셀렉트 가능
-  // mounted() {
-  //   console.group("lifcycle is mounted", document.querySelector("h1"));
-  // },
-  //메소드
   methods: {
-    test() {
-      console.log("함수호출");
+    change() {
+      this.isActive = !this.isActive;
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+h2.active {
+  color: green;
+}
+</style>
