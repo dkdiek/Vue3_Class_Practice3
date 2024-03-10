5149058255c11b66a00359a82113f4d9e37bddf9
k <developerlyk@gmail.com>
2024-03-10 21:01:16 +0900

    Options api prop사용

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,25 @@
 <template>
-  <button @click="changeMessage">{{ message }}</button>
-  {{ watchMessage }}
+  <childComponent
+    :sendProps1="title"
+    :sendProps2="createAt"
+    :sendProps3="obj"
+  />
 </template>
-
 <script>
+import ChildComponent from "./components/ChildComponent.vue";
 export default {
+  components: {
+    ChildComponent,
+  },
   data() {
     return {
-      message: "안녕하세요 watch기능테스트 오리지널 문구",
-      watchMessage: "",
+      title: "부모컴포넌트에서 선언된 데이터입니다.",
+      createAt: 2024,
+      obj: {
+        id: 2024,
+        name: "John",
+      },
     };
-  },
-  watch: {
-    /* message란 변수를 지켜보고있다 변경이 있으면 실행
-      데이터 뿐만 아니라 computed로 계산된 형태의 데이터도 watch로 감지할 수 있다.
-      보통 게시판에서 한 컬럼을 선택하였을때, 고유한 id값이 바뀜을 감지하고
-      이때, 그id값에 상세데이터를 호출할때 주로사용한다
-    */
-    message() {
-      this.watchMessage = "watch 동작";
-    },
-    id() {
-      //해당 상세데이터를 조회하는 api호출
-    },
-  },
-  methods: {
-    changeMessage() {
-      console.log("함수호출");
-      this.message = "변경된 message입니다";
-    },
   },
 };
 </script>
