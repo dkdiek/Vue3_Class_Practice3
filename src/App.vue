fba407cb238ef75b90b2ebd2144429e489b4f7d3
k <developerlyk@gmail.com>
2024-03-10 21:15:56 +0900

    Composition api prop사용

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,22 @@
     :sendProps3="obj"
   />
 </template>
-<script>
+<!-- 컴포지션 api는 setup이란게 붙어야함 -->
+<script setup lang="ts">
+import { reactive, ref } from "vue";
 import ChildComponent from "./components/ChildComponent.vue";
-export default {
-  components: {
-    ChildComponent,
-  },
-  data() {
-    return {
-      title: "부모컴포넌트에서 선언된 데이터입니다.",
-      createAt: 2024,
-      obj: {
-        id: 2024,
-        name: "John",
-      },
-    };
-  },
-};
+
+interface Obj {
+  id: number;
+  name: string;
+}
+
+const title = ref("부모컴포넌트에서 선언된 데이터입니다.");
+const createAt = ref<nubmer>(2024);
+const obj = reactive<Obj>({
+  id: 2024,
+  name: "john",
+});
 </script>
 
 <style></style>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -5,14 +5,29 @@
   <div>{{ sendProps3.name }}</div>
 </template>
 
-<script>
-export default {
-  props: {
-    sendProps1: String,
-    sendProps2: Number,
-    sendProps3: Object,
-  },
-};
+<script setup lang="ts">
+import { toRefs } from "vue";
+
+interface Obj {
+  id: number;
+  name: string;
+}
+
+//1.컴포지션 api에서 타입스크립트를 활용하는 방법
+//1-1 prop 받은 데이터 형식에 맞게 타입을 지정해준다
+
+//Prop 받은 데이터 이름 자체에 미리 선언해둔 타입을 설정해준다.
+interface Props {
+  sendProps1: String;
+  sendProps2: Number;
+  sendProps3: Obj;
+}
+
+//definProps toRefs 내장함수를 통해 Prop 받은 데이터를 활용하여 템플릿 부분에 호출하여 사용한다.
+/* define props는 import안해도 vue가 알아서 해준다 */
+const props = defineProps<Props>();
+//torefs를 안하면 template에서 props.sendProps1 이런식으로 써야한다
+const { sendProps1, sendProps2, sendProps3 } = toRefs(props);
 </script>
 
 <style></style>
